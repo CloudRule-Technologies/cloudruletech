c7a8f67834cda57947d3c134646bcb1e17a17312
sailendraprasath <sailendraprasath05@gmail.com>
2026-01-30 15:33:26 +0530

    this commit msg by sailesh

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      {/* Example usage here */}
+      <div className="text-5xl text-center bg-amber-200 p-4">Hey Team!!</div>
+      <h1>Hey team its another comment</h1>
+    </>
+  );
+};
+
+export default App;
