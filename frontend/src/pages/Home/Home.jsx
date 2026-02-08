f180da50e1eabd2d88db4bf6d309620216dac7c9
sailendraprasath <sailendraprasath05@gmail.com>
2026-02-08 14:32:42 +0530

    this is a final commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Hero from "../../components/Hero/Hero";
 import Feature from "../../components/Feature/Feature";
 import Cta from "../../components/Cta/Cta";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    document.title = "Home | CloudRule";
+  });
   return (
     <>
       <div data-testid="home-page">
